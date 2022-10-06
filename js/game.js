891e1df87fb7df9442390f2d473ddb50cf37c500
John Cardona <john@dycdigital.com.co>
2022-10-06 15:42:34 -0500

    Logic to choose the  player's warrior

--- js/game.js
+++ js/game.js
@@ -4,7 +4,23 @@ function startGame(){
 }
 
 function selectWarrior(){
-    
+
+    let shillingford = document.getElementById('shillingford')
+    let clayton = document.getElementById('clayton')
+    let frolova = document.getElementById('frolova')
+    let spanWarriorPlayerName = document.getElementById('warrior-player-name')
+    let spanWarriorEnemyName = document.getElementById('warrior-enemy-name')
+
+    if (shillingford.checked){
+        spanWarriorPlayerName.innerHTML = "Shillingford"
+    } else if (clayton.checked){
+        spanWarriorPlayerName.innerHTML = "Clayton"
+    } else if (frolova.checked){
+        spanWarriorPlayerName.innerHTML = "Frolova"
+    } else {
+        alert("Select one warrior")
+    }
+
 }
 
 window.addEventListener('load', startGame)
