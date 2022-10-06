a8d9d8dee5fc154d45c9b8b3a57b3d850de39512
John Cardona <john@dycdigital.com.co>
2022-10-06 16:55:47 -0500

    Added player attack and aleatory enemy attack

--- js/game.js
+++ js/game.js
@@ -1,15 +1,29 @@
+let playerAttack
+let enemyAttack 
+
+
 function startGame(){
     let buttonWarriorPlayer = document.getElementById('button-warrior')
-    buttonWarriorPlayer.addEventListener('click', selectWarrior)
+    buttonWarriorPlayer.addEventListener('click', selectWarriorPlayer)
+
+    let buttonAttackFire = document.getElementById('button-fire')
+    buttonAttackFire.addEventListener('click', playerAttackFire)
+
+    let buttonAttackWater = document.getElementById('button-water')
+    buttonAttackWater.addEventListener('click', playerAttackWater)
+
+    let buttonAttackMagic = document.getElementById('button-magic')
+    buttonAttackMagic.addEventListener('click', playerAttackMagic)
+
 }
 
-function selectWarrior(){
+function selectWarriorPlayer(){
 
     let shillingford = document.getElementById('shillingford')
     let clayton = document.getElementById('clayton')
     let frolova = document.getElementById('frolova')
     let spanWarriorPlayerName = document.getElementById('warrior-player-name')
-    let spanWarriorEnemyName = document.getElementById('warrior-enemy-name')
+    
 
     if (shillingford.checked){
         spanWarriorPlayerName.innerHTML = "Shillingford"
@@ -20,7 +34,52 @@ function selectWarrior(){
     } else {
         alert("Select one warrior")
     }
+    selectWarriorEnemy()
 
+}
+
+function random(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+    
+}
+
+function selectWarriorEnemy(){
+    let ramdonEnemy = random(1,3)
+    let spanWarriorEnemyName = document.getElementById('warrior-enemy-name')
+
+    if (ramdonEnemy == 1){
+        spanWarriorEnemyName.innerHTML = "Shillingford"
+    } else if (ramdonEnemy == 2){
+        spanWarriorEnemyName.innerHTML = "Clayton"
+    } else {
+        spanWarriorEnemyName.innerHTML = "Frolova"
+    }
+}
+
+function playerAttackFire(){
+    playerAttack = "Fire"
+    ramdomEnemyAttack()
+
+}
+function playerAttackWater(){
+    playerAttack = "Water"
+    ramdomEnemyAttack()
+
+}
+function playerAttackMagic(){
+    playerAttack = "Magic"
+    ramdomEnemyAttack()
+
+}
+function ramdomEnemyAttack(){
+    enemyAttack = random(1,3)
+    if (enemyAttack == 1){
+        enemyAttack = "Fire"
+    }else if (enemyAttack == 2) {
+        enemyAttack = "Water"
+    } else {
+        enemyAttack = "Magic"
+    }
 }
 
 window.addEventListener('load', startGame)
