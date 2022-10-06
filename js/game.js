3295a34ed6461b4002253bd069d7a1ace2f6e767
John Cardona <john@dycdigital.com.co>
2022-10-06 13:09:01 -0500

    Created the startGame function and selectWarrior function

---
+++ js/game.js
@@ -0,0 +1,10 @@
+function startGame(){
+    let buttonWarriorPlayer = document.getElementById('button-warrior')
+    buttonWarriorPlayer.addEventListener('click', selectWarrior)
+}
+
+function selectWarrior(){
+    
+}
+
+window.addEventListener('load', startGame)
